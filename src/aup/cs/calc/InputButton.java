8a7473fa87caee7bab5db179b9f6244655a62c23
DucOfAudubon <gideonvicini@gmail.com>
2021-04-18 21:58:25 +0200

    Hopefully this is just the Battle Project?

--- src/aup/cs/calc/InputButton.java
+++ src/aup/cs/calc/InputButton.java
@@ -15,7 +15,7 @@ public class InputButton extends Button{
      * that execute may get overwritten for other uses of input
      * @param value this is what's on the button
      * @param inProperty this is the textfield
-     * @param width how wide you want your button. Shocking that this isn't a default constructor field for button
+     * @param width how wide you want your button. Shocking that this isn't a default constructor option for button
      */
     public InputButton(String value, StringProperty inProperty, int width){
         super(value);
@@ -23,14 +23,11 @@ public class InputButton extends Button{
         this.inProperty = inProperty;
         setMinWidth(width);
         setStyle("-fx-background-color: #404040; -fx-text-fill: #ffffff; -fx-border-color: #282828");
-        setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                try{
-                    execute();
-                } catch (IllegalOperatorsException ioe){
-                    System.err.println("Illegal Operators Detected: " + ioe.getMessage());
-                }
+        setOnAction(event -> {
+            try{
+                execute();
+            } catch (IllegalOperatorsException ioe){
+                System.err.println("Illegal Operators Detected: " + ioe.getMessage());
             }
         });
     }
